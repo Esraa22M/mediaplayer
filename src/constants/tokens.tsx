8e4efdbb0e457542cdb85098710966e582fbe19b
Esraa22M <58557774+Esraa22M@users.noreply.github.com>
2025-09-13 22:26:26 +0300

    theming

---
+++ src/constants/tokens.tsx
@@ -0,0 +1,14 @@
+export const colors = {
+	primary: '#5a7480',
+	background:"#1e2025",
+
+	  text: '#dedcdc',
+
+	textMuted: '#9ca3af',
+        icon:"#8A7E78",
+
+	maximumTrackTintColor: 'rgba(255, 255, 255, 0.4)',
+    minmumTrackTintColor:'rgba(255 ,255 ,255 , 0.6 )'
+}
+export const fontSize = {xs:12, sm:16 ,base:20, lg:24}
+export const screenPadding = {horzintal:24}
