5061de2498379dc76d5281dc97fad70cbd3a6ab7
Esraa22M <58557774+Esraa22M@users.noreply.github.com>
2025-09-16 06:01:29 +0300

    handled search with debounce

--- src/components/tracks-list.tsx
+++ src/components/tracks-list.tsx
@@ -6,17 +6,19 @@ import {type Track } from './track-list-item';
 import { screenPadding } from '@/constants/tokens';
 import { utilsStyles } from '@/styles';
 type TraksListProps = Partial<FlatListProps<any>>;
-const { width, height} = Dimensions.get("window");
-
+const { width} = Dimensions.get("window");
+import { useNavigationSearch } from '@/hooks/useNavigationSearch';
 const ItemDivider = () => {
 	
 	return <View style={{...utilsStyles.itemSeparator, marginVertical:20 , marginLeft:0.2*width}} />}
 export const TracksListComponent = ({ ...flatlistProps }: TraksListProps) => {
+	const [searchTerm]= useNavigationSearch({searchBarOptions:{placeholder:"Cue the track"}})
 	return (
 	
 			<FlatList
 				data={library}
 				ItemSeparatorComponent={ItemDivider}
+				ListFooterComponent={ItemDivider}
 				style={{paddingHorizontal:screenPadding.horzintal}}
 				renderItem={({ item: track }) => {
 					return <TrackListItem track={{ ...track, title: track?.title, image: track?.artwork }} />
--- src/hooks/useNavigationSearch.tsx
+++ src/hooks/useNavigationSearch.tsx
@@ -1,19 +1,28 @@
 import { SearchBarProps } from 'react-native-screens'
 import { useNavigation } from 'expo-router'
-import { useLayoutEffect, useState } from 'react'
+import { useLayoutEffect, useState , useEffect} from 'react'
 import { colors } from '@/constants/tokens'
 
 const defaultSearchOptions: SearchBarProps = { tintColor: colors.primary, hideWhenScrolling: false }
 export const useNavigationSearch = ({
 	searchBarOptions,
+	debounceDelay = 500,
 }: {
 	searchBarOptions?: SearchBarProps
+	debounceDelay?: number
 }) => {
 	const [searchTerm, setSearchTerm] = useState('')
 	const navigation = useNavigation()
+	const [debouncedTerm, setDebouncedTerm] = useState('')
 	const handleTextChange: SearchBarProps['onChangeText'] = ({ nativeEvent: { text } }) => {
 		setSearchTerm(text)
 	}
+	useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedTerm(searchTerm)
+        }, debounceDelay);
+        return ()=>{clearTimeout(handler)}
+	}, [searchTerm, debounceDelay])
 	useLayoutEffect(() => {
 		navigation.setOptions({
 			headerSearchBarOptions: {
@@ -23,5 +32,5 @@ export const useNavigationSearch = ({
 			},
 		})
 	}, [navigation, searchBarOptions])
-	return searchTerm
+	return { searchTerm,debouncedTerm, setSearchTerm }
 }
